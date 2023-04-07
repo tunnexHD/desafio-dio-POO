50d55b9bce7e78929d659bc395d9aca2911e7ca5
Tunnex <tunnexca@gmail.com>
2023-04-07 15:49:56 -0300

    Finalizando o Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,59 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("Curso Java");
-        curso1.setDescricao("Descrição Curso Java");
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("Curso Python");
-        curso2.setDescricao("Descrição Curso Python");
-        curso2.setCargaHoraria(7);
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria Python");
-        mentoria.setDescricao("Descrição Mentoria Python");
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev devMatheus = new Dev();
+        devMatheus.setNome("Matheus");
+        devMatheus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Matheus:" + devMatheus.getConteudosInscritos());
+        devMatheus.progredir();
+        devMatheus.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Matheus:" + devMatheus.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Matheus:" + devMatheus.getConteudosConcluidos());
+        System.out.println("XP:" + devMatheus.calcularTotalXp());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+        System.out.println("-------");
 
-        System.out.println(mentoria);
+        Dev devTunnex = new Dev();
+        devTunnex.setNome("Joao");
+        devTunnex.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Tunnex:" + devTunnex.getConteudosInscritos());
+        devTunnex.progredir();
+        devTunnex.progredir();
+        devTunnex.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Tunnex:" + devTunnex.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Tunnex:" + devTunnex.getConteudosConcluidos());
+        System.out.println("XP:" + devTunnex.calcularTotalXp());
+
     }
+
 }
